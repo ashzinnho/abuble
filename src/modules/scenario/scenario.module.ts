61f5e4701800490f23aff129ce46d788e6244a7e
ashzinnho <analuapamm@gmail.com>
2025-09-01 10:54:11 -0400

    sss

--- src/modules/scenario/scenario.module.ts
+++ src/modules/scenario/scenario.module.ts
@@ -1,9 +1,16 @@
-import { Module } from '@nestjs/common';
+import { Module, Logger } from '@nestjs/common';
 import { ScenarioService } from './scenario.service';
 import { ScenarioController } from './scenario.controller';
+import { ShareModule } from 'src/shared/shared.module';
+import * as UseCases from './use-cases';
+import * as Repositories from './repository';
+
+const useCases = Object.values(UseCases);
+const repositories = Object.values(Repositories);
 
 @Module({
+  imports: [ShareModule],
   controllers: [ScenarioController],
-  providers: [ScenarioService],
+  providers: [ScenarioService, ...useCases, ...repositories, Logger],
 })
 export class ScenarioModule {}
