1abc6d395929f3c9c3c7f31c3fd61788e77df0c9
ashzinnho <analuapamm@gmail.com>
2025-09-02 12:23:22 -0400

    llllll

--- src/modules/scenario/scenario.service.ts
+++ src/modules/scenario/scenario.service.ts
@@ -2,16 +2,17 @@ import { Injectable } from '@nestjs/common';
 import { CreateScenarioDto } from './dto/create-scenario.dto';
 import { UpdateScenarioDto } from './dto/update-scenario.dto';
 import { CreateScenarioUseCase } from './use-cases/create-scenario.use-case';
+import { ListScenarioUseCase } from './use-cases';
 
 @Injectable()
 export class ScenarioService {
-  constructor(private readonly createScenarioUseCase: CreateScenarioUseCase){}
+  constructor(private readonly createScenarioUseCase: CreateScenarioUseCase, private readonly listScenarioUseCase: ListScenarioUseCase){}
   create(data: CreateScenarioDto) {
   return this.createScenarioUseCase.execute(data);
   }
 
   findAll() {
-    return `This action returns all scenario`;
+    return this.listScenarioUseCase.list();
   }
 
   findOne(id: number) {
